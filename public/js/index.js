a0ff3e716a62f0078b3fb798cf3c3b07bf044c95
pratibha <pratibhaagarkar@gmail.com>
2019-04-17 17:00:51 +0530

    create new user and alert other

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,12 @@
 var socket = io();
-socket.on('connect' ,function() {
+socket.on('connect' , function() {
   console.log("connected to server");
 
-
-
-  /*socket.emit('createEmail', {
-    to:'reva@ema.com',
-    text:'hi'
-  });
-
-  socket.on('newEmail',function(email){
-    console.log('new email',email);
-  });
-
-  */
 });
-socket.on('disconnect' ,function()  {
+socket.on('disconnect' , function()  {
   console.log("disconnected from server");
 });
 
-socket.on('newMessage',function(message){
-  console.log('new message',message);
+socket.on('newMessage', function(message) {
+  console.log('new message', message);
 });
--- server/server.js
+++ server/server.js
@@ -14,26 +14,32 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log("New user connected");
 
-  socket.on('createMessage',(message)=>{
-    console.log('createMessage',message);
-    io.emit('newMessage',{
-      from: message.from,
-      text: message.text,
-      createdAt:newDate().getTime()
-    });
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
   });
 
-/*
-socket.emit('newEmail',{
-  from:'pa@gmail.com',
-  text:'hey,whats going on',
-  createdAt:123
-});
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
 
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt:new Date().getTime()
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  });
 
-socket.on('createEmail',(newEmail)=>{
-  console.log(" createEmail",newEmail);
-});*/
 
 socket.on('disconnect', () => {
   console.log(" user disconnected");
