fcdf3c2258a02b7f3d241e44c2f23f17f8fd2c7c
pratibha <pratibhaagarkar@gmail.com>
2019-04-21 12:08:09 +0530

    add geolocation support to chat app

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage,generateLocationMessage} = require('./message');
 
 describe('generateMessage',() => {
    it('should generate correct message object', () => {
@@ -10,6 +10,20 @@ describe('generateMessage',() => {
 
      expect(message.createdAt).toBeA('number');
      expect(message).toInclude({from,text});
+   })
+
+});
+
+describe('generateLocationMessage',() => {
+   it('should generate correct location object', () => {
+     var from = 'nik';
+     var latitude = '18';
+     var longitude = '73';
+     var url ='https://www.google.com/maps?q=18,73';
+     var message = generateLocationMessage(from,latitude,longitude);
+
+     expect(message.createdAt).toBeA('number');
+     expect(message).toInclude({from,url});
    })
 
 });
